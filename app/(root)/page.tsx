6bfb170db656ed7cc760cfa9b8a307c32eacdb04
Paul Bailey <psbailey31@gmail.com>
2024-06-20 00:49:34 +0100

    correct pagination

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -61,9 +61,9 @@ export default async function Home({ searchParams }: SearchParamProps) {
           emptyTitle="No Events Found"
           emptyStateSubtext="Please come back later"
           collectionType="All_Events"
-          limit={6}
-          page={1}
-          totalPages={2}
+          limit={3}
+          page={page}
+          totalPages={events?.totalPages}
           />
 
       </section>
